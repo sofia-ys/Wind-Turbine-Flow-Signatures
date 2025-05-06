7c9421c65150986edd5df9a942c51b6c42a81383
janwan11 <140969539+janwan11@users.noreply.github.com>
2025-05-06 09:18:31 +0200

    Update PlotMomentumSurgingVsFixed.m

--- codeAndData/codeAndData/PlotMomentumSurgingVsFixed.m
+++ codeAndData/codeAndData/PlotMomentumSurgingVsFixed.m
@@ -1,6 +1,51 @@
+clear all
+warning('off', 'all');
+folder = "C:\Users\janwa\Documents\TU Delft\Bsc2\Test simulation\Code\codeAndData\NREL_FXXXXX_5D_000_00025_copy\exportedCSV\phase_average\";
+planes = [2, 5, 8];
+momentum_case = 'Fixed';
+
+momentum_fixed = zeros(3, 24);
+instances = 24;
+for i = 1:length(planes)
+   file = (folder +'PhaAvg_V_comp_' + momentum_case+ '_xPlane'+num2str(planes(i))+"D.csv");
+   data = readtable(file);
+   if i == 1
+       col1 = data{:, end-1};  % Second to last column
+       col2 = data{:, end};    % Last column
+
+       distance = sqrt(col1.^2 + col2.^2);
+       rows_to_keep = find(distance <= 126);
+   end
+   for ii = 1:instances
+       if ii == 1 & i == 1
+            data = data(rows_to_keep, :);
+       end
+       momentum_fixed(i,ii) = sum(data{:,(3*ii-2)});
+   end
+end
+momentum_surging = zeros(3, 24);
+momentum_case = 'Surging';
+for i = 1:length(planes)
+   file = (folder +'PhaAvg_V_comp_' + momentum_case+ '_xPlane'+num2str(planes(i))+"D.csv");
+   data = readtable(file);
+   if i == 1
+       col1 = data{:, end-1};  % Second to last column
+       col2 = data{:, end};    % Last column
+
+       distance = sqrt(col1.^2 + col2.^2);
+       rows_to_keep = find(distance <= 126);
+   end
+   for ii = 1:instances
+       if ii == 1 & i == 1
+            data = data(rows_to_keep, :);
+       end
+       momentum_surging(i,ii) = sum(data{:,(3*ii-2)});
+   end
+end
+
 % Example arrays - mock data 
-fixedData = rand(3, 24);   % Example random data for fixed case (3 planes, 24 phases)
-surgingData = rand(3, 24); % Example random data for surging case (3 planes, 24 phases)
+fixedData = momentum_fixed;   % Example random data for fixed case (3 planes, 24 phases)
+surgingData = momentum_surging; % Example random data for surging case (3 planes, 24 phases)
 
 % Phase angles from 0 to 2*pi (24 phases)
 phase = linspace(0, 2*pi, 24);  % 24 values from 0 to 2*pi
